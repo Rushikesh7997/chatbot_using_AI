69703cea878ca7f33cf05a0b4cad4c989fb8ceb4
Rushikesh <pardhikar26@gmail.com>
2025-03-26 01:39:05 +0530

    work on UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
 import './App.css'
+import { ChatbotIcon } from './components/ChatbotIcon'
 
 function App() {
 
   return (
     <>
-      <h1>Chat Bot Using Gemini Ai </h1>
+      <div className="container">
+        <div className="chatbot-popup">
+          <div className="chatbot-header">
+            <div className="header-info">
+              <ChatbotIcon/>
+              <h2 className="logo-text">
+                Chatbot
+              </h2>
+            </div>
+            <button className="material-symbols-outlined">keyboard_arrow_down</button>
+          </div>
+
+          <div className="chat-body">
+            <div className="message bot-message">
+              <ChatbotIcon/>
+              <p className="message-text">
+                Hey There ✨, <br /> How can I help you today ?
+              </p>
+            </div>
+            <div className="message user-message">
+              <ChatbotIcon/>
+              <p className="message-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+
+          <div className="chat-footer">
+            <form action="" className="chat-form">
+              <input type="text" placeholder='Message Here....' className="message-input" required />
+              <button className="material-symbols-outlined">arrow_upward</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
