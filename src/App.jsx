ec43d90c06dcd50bb009fafbd30a95e5a91e7bc5
Rushikesh <pardhikar26@gmail.com>
2025-03-28 03:51:27 +0530

    done api fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { ChatbotIcon } from './components/ChatbotIcon'
 import ChatForm from './components/ChatForm'
 import { ChatMessage } from './components/ChatMessage';
 
+
 function App() {
   const [chatHistory, setChatHistory] = useState([]);
+  const [showChatbot, setShowChatbot] = useState(false);
+  const chatBodyRef = useRef();
 
   const generateBotResponse = async(history) =>{
+
+    const updateHistory = (text, isError = false) =>{
+      setChatHistory(prev=>[...prev.filter(msg=>msg.text !=="Thinking..."),{role:"model", text, isError}])
+    }
+
     history = history.map(({role,text})=>({role,parts:[{text}]}));
 
     const requestOption = {
@@ -17,19 +25,30 @@ function App() {
     }
 
     try{
-      const response = await fetch(import.meta.env.API_URL, requestOption);
+      const response = await fetch(import.meta.env.VITE_API_URL, requestOption);
       const data = await response.json();
       if(!response.ok) throw new Error(data.error.message || "Something went wrong!")
         
-      console.log(data)
+      const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      updateHistory(apiResponseText);  
     }catch(error){
-      console.log(error)
+      updateHistory(error.message, true)
     }
   }
+
+  useEffect(()=>{
+    chatBodyRef.current.scrollTo({top:chatBodyRef.current.scrollHeight, behavior:"smooth"});
+  },[chatHistory])
   
   return (
     <>
-      <div className="container">
+      <div className={`container ${showChatbot ? "show-chatbot" : ""}`}>
+
+        <button onClick={()=>setShowChatbot((prev)=>!prev)} id="chatbot-toggler">
+          <span className='material-symbols-outlined'>mode_comment</span>
+          <span className='material-symbols-outlined'>close</span>
+        </button>
+
         <div className="chatbot-popup">
           <div className="chatbot-header">
             <div className="header-info">
@@ -38,10 +57,10 @@ function App() {
                 Chatbot
               </h2>
             </div>
-            <button className="material-symbols-outlined">keyboard_arrow_down</button>
+            <button onClick={()=>setShowChatbot((prev)=>!prev)}  className="material-symbols-outlined">keyboard_arrow_down</button>
           </div>
 
-          <div className="chat-body">
+          <div ref={chatBodyRef} className="chat-body">
             <div className="message bot-message">
               <ChatbotIcon/>
               <p className="message-text">
